a0d4138bc280ab97b6f19f5faed82ff34cebeb16
shubham palwade <244582809+shubhampalwade2006-source@users.noreply.github.com>
2026-04-24 23:05:07 +0530

    Add Shape class with Rectangle and Circle implementations
    Implemented Shape class hierarchy with Rectangle and Circle.

---
+++ Experiment09.cpp
@@ -0,0 +1,98 @@
+#include<iostream>
+using namespace std;
+class Shape
+{
+    public:
+ // Pure virtual function (makes this class abstract) 
+virtual void area() = 0; 
+// Another pure virtual function 
+virtual void perimeter() = 0; 
+}; 
+
+// Derived class: Rectangle 
+class Rectangle : public Shape
+{
+    private: 
+
+float length, width; 
+public: 
+// Function to accept rectangle dimensions 
+void getData() 
+{
+cout<<"Enter length of rectangle:";
+cin>>length;
+cout<<"Enter width of rectangle:";
+cin>>width;
+}
+
+//Override pure virtual function to calculate area
+void area()
+{
+    cout <<"Area of rectangle="<<length*width<<endl;
+
+}
+//Override pure virtual function to calculate perimeter
+void perimeter()
+{
+    cout<<"Perimeter of rectangle="<<2*(length+width)<<endl;
+
+}
+};
+
+//Derived class:Circle
+class Circle: public Shape
+{
+   private:
+   float radius;
+   
+   public:
+   void getData()
+   {
+    cout<<"Enter radius of circle:";
+    cin>>radius;
+   }
+   void area()
+   {
+    cout<<"Area of circle="<<3.1416*radius*radius<<endl;
+
+   }
+   void perimeter()
+   {
+
+    cout<<"perimeter of circle="<<2*3.1416*radius<<endl;
+   }
+};
+// Main function
+int main()
+{
+    Rectangle rect;
+    Circle circ;
+
+    cout<<"--Rectangle--\n";
+    rect.getData();
+    rect.area();
+    rect.perimeter();
+
+    cout<<"\n--Circle--\n";
+    circ.getData();
+    circ.area();
+    circ.perimeter();
+
+    return 0;
+}
+
+
+
+
+OUTPUT:
+
+--Rectangle--
+Enter length of rectangle:2
+Enter width of rectangle:2
+Area of rectangle=4
+Perimeter of rectangle=8
+
+--Circle--
+Enter radius of circle:3
+Area of circle=28.2744
+perimeter of circle=18.8496
